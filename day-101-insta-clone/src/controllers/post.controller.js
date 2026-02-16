ee3a20c98750e04d4a704d3457f163ccc8bac9a5
Ritik Kumar <ritik.kumar22@outlook.com>
2026-02-16 21:49:32 +0530

    Fetch Post Done

--- day-101-insta-clone/src/controllers/post.controller.js
+++ day-101-insta-clone/src/controllers/post.controller.js
@@ -14,11 +14,11 @@ async function createPostController (req, res) {
 
     if (!token) {
         return res.status(401).json({
-            message: "Token not provided, Unauthorized access"
+            message: "Token not provided, Unauthorized Access"
         })
     }
 
-    let decoded = null
+    let decoded;
 
     try{
         decoded = jwt.verify(token, process.env.JWT_SECRET)
@@ -48,7 +48,88 @@ async function createPostController (req, res) {
     })
 }
 
+async function getPostController (req, res) {
+
+    const token = req.cookies.token
+
+    if (!token) {
+        return res.status(401).json({
+            message: "UnAuthorized Access"
+        })
+    }
+
+    let decoded;
+
+    try{
+        decoded = jwt.verify(token, process.env.JWT_SECRET)
+    } catch (err) {
+        res.status(401).json({
+            message: "Invalid Token"
+        })
+    }
+
+    const userId = decoded.id
+
+    const posts = await postModel.find({
+        user: userId
+    })
+
+    res.status(200).json({
+        message: "Posts fetched successfully",
+        posts
+    })
+
+
+}
+
+async function getPostDetailsController (req, res) {
+
+    const token = req.cookies.token
+
+    if (!token) {
+        return res.status(401).json({
+            message: "UnAuthorized Access"
+        })
+    }
+
+    let decoded;
+
+    try{
+        decoded = jwt.verify(token, process.env.JWT_SECRET)
+    } catch (err) {
+        res.status(401).json({
+            message: "Invalid Token"
+        })
+    }
+
+    const userId = decoded.id
+    const postId = req.params.postId
+
+    const post = await postModel.findById(postId)
+
+    if (!post) {
+        return res.status(404).json({
+            message: "Post not found."
+        })
+    }
+
+    const isValidUser = post.user.toString() === userId
+
+    if(!isValidUser) {
+        return res.status(403).json({
+            message: "Forbidden Content"
+        })
+    }
+
+    res.status(200).json({
+        message: "Post fetched successfully",
+        post
+    })
+}
+
+
 module.exports = {
     createPostController,
-
+    getPostController,
+    getPostDetailsController
 }
--- day-101-insta-clone/src/routes/post.routes.js
+++ day-101-insta-clone/src/routes/post.routes.js
@@ -7,7 +7,16 @@ const postRouter = express.Router();
 
 // POST - /api/posts [proctected route]
 // req.body - {caption, img-file}
-
 postRouter.post("/", upload.single("image"), postController.createPostController)
+
+
+// GET - /api/posts/ [protected route]
+postRouter.get("/", postController.getPostController)
+
+
+// GET - /api/posts/details/:postId
+// return detail about specific post with the id. also check whether the post belongs to the user that the request come from.
+postRouter.get("/details/:postId", postController.getPostDetailsController)
+
 
 module.exports = postRouter;
