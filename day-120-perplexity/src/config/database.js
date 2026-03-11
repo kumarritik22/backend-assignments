85136ef4ecfe397eab3ea4438bbe41575f07ae92
Ritik Kumar <ritik.kumar22@outlook.com>
2026-03-11 23:37:17 +0530

    initialized perplexity project with user registration and email notification using nodemailer

---
+++ day-120-perplexity/src/config/database.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+export function connectToDB() {
+    mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("Connected to Database");
+    })
+    .catch(err => {
+        console.log("Error connecting to DB", err)
+    }) 
+
+}
