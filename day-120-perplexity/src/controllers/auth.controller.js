0ed38254173e894c208bd7eeee941d505b41497a
Ritik Kumar <ritik.kumar22@outlook.com>
2026-03-12 21:56:48 +0530

    added email verification and login functionality

--- day-120-perplexity/src/controllers/auth.controller.js
+++ day-120-perplexity/src/controllers/auth.controller.js
@@ -2,6 +2,11 @@ import userModel from "../models/user.model.js";
 import jwt from "jsonwebtoken";
 import { sendEmail } from "../services/mail.service.js";
 
+
+// @route POST /api/auth/register
+// @description Register a new user
+// @access Public
+// @body {username, email, password}
 export async function register(req, res) {
     const { username, email, password} = req.body
 
@@ -19,12 +24,19 @@ export async function register(req, res) {
 
     const user = await userModel.create({username, email, password})
 
+    const emailVerificationToken = jwt.sign({
+        email: user.email
+    }, process.env.JWT_SECRET)
+
     await sendEmail({
         to: email,
         subject: "Welcome to Perplexity!",
         html: `
                 <p>Hi ${username},</p>
                 <p>Thank you for registering at <strong>Perplexity</strong>. We're excited to have you on board!</p>
+                <p>Please verify your email address by clicking the link below:</p>
+                <a href="http://localhost:3000/api/auth/verify-email?token=${emailVerificationToken}">Verify Email</a>
+                <p>If you did not create an account, please ignore this email.</p>
                 <p>Best regards,<br>The Perplexity Team</p>
         `
     })
@@ -41,3 +53,123 @@ export async function register(req, res) {
 
 }
 
+
+// @route POST /api/auth/login
+// @description Login a user and return jwt token
+// @access public
+// @body {email, password}
+export async function login(req, res) {
+    const { email, password } = req.body
+
+    const user = await userModel.findOne({email})
+
+    if (!user) {
+        return res.status(400).json({
+            message: "Invalid email or password",
+            success: false,
+            err: "User not found"
+        })
+    }
+
+    const isPasswordMatch = await user.comparePassword(password);
+
+    if (!isPasswordMatch) {
+        return res.status(400).json({
+            message: "Invalid email or password",
+            success: false,
+            err: "Incorrect password"
+        })
+    }
+
+    if (!user.verified) {
+        return res.status(400).json({
+            message: "Please verify your email before logging in",
+            success: false,
+            err: "Email not verified"
+        })
+    }
+
+    const token = jwt.sign({
+        id: user._id,
+        username: user.username
+    }, process.env.JWT_SECRET, {expiresIn: "7d"})
+
+    res.cookie("token", token)
+
+    res.status(200).json({
+        message: "User logged in successfully",
+        success: true,
+        user:{
+            id: user._id,
+            username: user.username,
+            email: user.email
+        }
+    })
+}
+
+
+// @route GET /api/auth/get-me
+// @description Get currect logged in user's details
+// @access private
+export async function getMe(req, res) {
+    const userId = req.user.id;
+
+    const user = await userModel.findById(userId).select("-password");
+
+    if (!user) {
+        return res.status(404).json({
+            message: "User not found",
+            success: false,
+            err: "User not found"
+        })
+    }
+
+    res.status(200).json({
+        message: "User details fetched successfully",
+        success: true,
+        user
+    })
+}
+
+
+// @route GET /api/auth/verify-email
+// @description to verify user's email id
+// @access public
+// @query {token}
+export async function verifyEmail(req, res) {
+    const { token } = req.query
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+        const user = await userModel.findOne({email: decoded.email})
+
+        if (!user) {
+            return res.status(400).json({
+                message: "Invalid token",
+                sucess: false,
+                err: "User not found"
+            })
+        }
+
+        user.verified = true;
+
+        await user.save();
+
+        const html = 
+        `
+            <h1>Email verified successfully!</h1>
+            <p>Your email has been verified. You can now login to your account.</p>  
+            <a href="http://localhost:3000/login">Go to login</a> 
+        `
+
+        return res.send(html);
+
+    } catch(err) {
+        return res.status(400).json({
+            message: "Invalid or expired token",
+            success: false,
+            err: err.message
+        })
+    }
+}
--- day-120-perplexity/src/routes/auth.routes.js
+++ day-120-perplexity/src/routes/auth.routes.js
@@ -1,13 +1,35 @@
 import { Router } from "express";
-import { register } from "../controllers/auth.controller.js";
-import { registerValidator } from "../validators/auth.validator.js";
+import { register, verifyEmail, login, getMe } from "../controllers/auth.controller.js";
+import { registerValidator, loginValidator } from "../validators/auth.validator.js";
+import { authUser } from "../middlewares/auth.middleware.js";
 
 const authRouter = Router();
+
 
 // @route POST /api/auth/register
 // @description Register a new user
 // @access Public
 // @body {username, email, password}
 authRouter.post("/register",registerValidator, register);
+
+
+// @route POST /api/auth/login
+// @description Login a user and return jwt token
+// @access public
+// @body {email, password}
+authRouter.post("/login", loginValidator, login)
+
+
+// @route GET /api/auth/get-me
+// @description Get currect logged in user's details
+// @access private
+authRouter.get("/get-me", authUser, getMe)
+
+
+// @route GET /api/auth/verify-email
+// @description to verify user's email id
+// @access public
+// @query {token}
+authRouter.get("/verify-email", verifyEmail)
 
 export default authRouter;
