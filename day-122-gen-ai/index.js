4b1d0cb8d905e1e8f452bb6aa75f96e0c19078d6
Ritik Kumar <ritik.kumar22@outlook.com>
2026-03-14 22:25:09 +0530

    Add email sending functionality with nodemailer and integrate into AI agent

--- day-122-gen-ai/index.js
+++ day-122-gen-ai/index.js
@@ -1,7 +1,23 @@
 import "dotenv/config";
 import readline from "readline/promises";
 import { ChatMistralAI } from "@langchain/mistralai";
-import { HumanMessage } from "langchain";
+import { HumanMessage, tool, createAgent } from "langchain";
+import { sendEmail } from "./mail.service.js";
+import * as z from "zod";
+
+
+const emailTool = tool(
+    sendEmail,
+    {
+        name: "emailTool",
+        description: "Use this tool to send an email",
+        schema: z.object({
+            to: z.string().describe("The recipient's email address"),
+            html: z.string().describe("The HTML content of the email"),
+            subject: z.string().describe("The subject of the email")
+        })
+    }
+)
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -12,6 +28,11 @@ const model = new ChatMistralAI({
     model: "mistral-small-latest"
 });
 
+const agent = createAgent({
+    model,
+    tools: [emailTool]
+})
+
 const messages = []
 
 while(true) {
@@ -19,12 +40,12 @@ while(true) {
 
     messages.push(new HumanMessage(userInput))
 
-    const response = await model.invoke(messages)
+    const response = await agent.invoke({
+        messages
+    })
 
-    messages.push(response)
+    messages.push(response.messages[response.messages.length - 1])
 
-    console.log(`\x1b[34m[AI]\x1b[0m ${response.content}`)
+    console.log(`\x1b[34m[AI]\x1b[0m ${response.messages[response.messages.length - 1 ].content}`)
 }
-
-rl.close();
 
