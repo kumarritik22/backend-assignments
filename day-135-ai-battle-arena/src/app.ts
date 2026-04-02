429154696f5a86264ef3c387d2317802db0c4880
Ritik Kumar <ritik.kumar22@outlook.com>
2026-04-02 13:26:45 +0530

    added langchain and zod dependencies; enhance graph AI service with new state management and judging functionality

--- day-135-ai-battle-arena/src/app.ts
+++ day-135-ai-battle-arena/src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import useGraph from "./services/graph.ai.service.js";
 
 const app = express()
 
@@ -6,6 +7,10 @@ const app = express()
 // @route GET /health
 app.get("/health", (req, res) => {
     res.status(200).json({status: "Ok"})
+});
+
+app.post("/use-graph", async (req, res) => {
+    await useGraph("write a factorial function in javascript?")
 })
 
 export default app
