075006b22eaf7ea740950ce8e42c7f189a1560e0
Ritik Kumar <ritik.kumar22@outlook.com>
2026-05-24 19:04:38 +0530

    feat: add routing with react-router and create Register page

--- day-141-snitch/frontend/src/app/App.jsx
+++ day-141-snitch/frontend/src/app/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import { RouterProvider } from "react-router";
+import { routes } from "./app.routes.jsx";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-2xl">Hello Ramayan</h1>
-    </div>
+    <>
+      <RouterProvider router={routes} />
+    </>
   )
 }
 
