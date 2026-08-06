3b404fb7d233107eecfa4a555b69f0c24f9c66ce
Ritik Kumar <ritik.kumar22@outlook.com>
2026-08-06 15:54:13 +0530

    feat: add getSellerProducts endpoint to retrieve products for authenticated sellers

--- day-141-snitch/backend/src/controllers/product.controller.js
+++ day-141-snitch/backend/src/controllers/product.controller.js
@@ -31,3 +31,15 @@ export async function createProduct(req, res) {
     })
 }
 
+export async function getSellerProducts(req, res) {
+    const seller = req.user
+
+    const products = await productModel.find({seller: seller._id});
+
+    res.status(200).json({
+        message: "Product fetched successfully",
+        success: true,
+        products
+    });
+}
+
--- day-141-snitch/backend/src/routes/product.routes.js
+++ day-141-snitch/backend/src/routes/product.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { authenticateSeller } from "../middlewares/auth.middleware.js";
-import { createProduct } from "../controllers/product.controller.js";
+import { createProduct, getSellerProducts } from "../controllers/product.controller.js";
 import multer from "multer";
 import { createProductValidator } from "../validators/product.validator.js";
 
@@ -13,6 +13,18 @@ const upload = multer({
     }
 });
 
-router.post("/", authenticateSeller, createProductValidator, upload.array("images", 7), createProduct);
+// @route POST /api/products
+// @description create a new product 
+// @access private (seller only)
+
+router.post("/", authenticateSeller, upload.array("images", 7), createProductValidator, createProduct);
+
+
+// @route GEt /api/products/seller
+// @description Get all the products of authenticated products
+// @access private (seller only)
+
+router.get("/seller", authenticateSeller, getSellerProducts);
+
 
 export default router;
