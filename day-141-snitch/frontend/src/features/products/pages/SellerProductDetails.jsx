028d2c5c431f8f15745a08f675e1ebf9560319f8
Ritik Kumar <ritik.kumar22@outlook.com>
2026-08-09 19:37:43 +0530

    feat: implement product details fetching in SellerProductDetails component

--- day-141-snitch/frontend/src/features/products/pages/SellerProductDetails.jsx
+++ day-141-snitch/frontend/src/features/products/pages/SellerProductDetails.jsx
@@ -1,6 +1,28 @@
-import React from 'react'
+import { useState } from 'react'
+import { useParams } from 'react-router'
+import { useProduct } from '../hooks/useProduct'
+import { useEffect } from 'react'
 
 const SellerProductDetails = () => {
+
+    const [product, setProduct] = useState(null)
+
+    const {productId} = useParams()
+
+    const {handleGetProductById} = useProduct()
+
+    async function fetchProductDetails() {
+        const data = await handleGetProductById(productId)
+        setProduct(data)
+    }
+
+    useEffect(() => {
+      fetchProductDetails()
+    }, [productId])
+
+    console.log(product);
+    
+
   return (
     <div>
       SellerProductDetails
