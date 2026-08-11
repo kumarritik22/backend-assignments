3915c74bfee0bdaac5384a1e569d9a95729ea7f9
Ritik Kumar <ritik.kumar22@outlook.com>
2026-08-11 18:37:54 +0530

    feat: implement get cart functionality to retrieve user's cart with populated product details

--- day-141-snitch/backend/src/controllers/cart.controller.js
+++ day-141-snitch/backend/src/controllers/cart.controller.js
@@ -70,3 +70,20 @@ export const addToCart = async (req, res) => {
         success: true
     }) 
 }
+
+export const getCart = async (req, res) => {
+
+    const user = req.user
+
+    let cart = await cartModel.findOne({user: user._id}).populate("items.product");
+
+    if (!cart) {
+        cart = await cartModel.create({user: user._id})
+    }
+
+    return res.status(200).json({
+        message: "Cart fetched successfully",
+        success: true,
+        cart
+    })
+}
--- day-141-snitch/backend/src/routes/cart.routes.js
+++ day-141-snitch/backend/src/routes/cart.routes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { authenticateUser } from "../middlewares/auth.middleware.js";
 import { validateAddToCart } from "../validators/cart.validator.js";
-import { addToCart } from "../controllers/cart.controller.js";
+import { addToCart, getCart } from "../controllers/cart.controller.js";
 
 const router = express.Router();
 
 router.post("/add/:productId/:variantId", authenticateUser, validateAddToCart, addToCart);
+
+router.get("/", authenticateUser, getCart)
 
 
 export default router;
