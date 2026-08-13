302fc8c2c7b4869cb9bd9dca5bc377372fc70ce0
Ritik Kumar <ritik.kumar22@outlook.com>
2026-08-13 16:42:37 +0530

    feat: Build api to implement increment cart item quantity functionality with validation

--- day-141-snitch/backend/src/routes/cart.routes.js
+++ day-141-snitch/backend/src/routes/cart.routes.js
@@ -1,13 +1,15 @@
 import express from "express";
 import { authenticateUser } from "../middlewares/auth.middleware.js";
-import { validateAddToCart } from "../validators/cart.validator.js";
-import { addToCart, getCart } from "../controllers/cart.controller.js";
+import { validateAddToCart, validateIncrementCartItemQuantity } from "../validators/cart.validator.js";
+import { addToCart, getCart, incrementCartItemQuantity } from "../controllers/cart.controller.js";
 
 const router = express.Router();
 
 router.post("/add/:productId/:variantId", authenticateUser, validateAddToCart, addToCart);
 
-router.get("/", authenticateUser, getCart)
+router.get("/", authenticateUser, getCart);
+
+router.patch("/quantity/increment/:productId/:variantId", authenticateUser, validateIncrementCartItemQuantity, incrementCartItemQuantity);
 
 
 export default router;
