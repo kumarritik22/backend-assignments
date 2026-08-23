187445a2efa4fc7eca2305558bdba7d6f5dcbc48
Ritik Kumar <ritik.kumar22@outlook.com>
2026-08-23 13:37:48 +0530

    Add backend setup with Express and Nodemon and create docker image and container

---
+++ day-157-docker-compose/backend/src/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+
+const app = express()
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.status(200).json({
+        message: "Hello Team"
+    })
+})
+
+app.get("/api/data", (req, res) => {
+    const data = {
+        id: 1, 
+        name: "Ramayan",
+        director: "Nitesh Tiwari"
+    };
+
+    res.status(200).json(data);
+})
+
+export default app;
