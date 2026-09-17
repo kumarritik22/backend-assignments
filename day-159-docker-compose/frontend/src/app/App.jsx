05be15e54819d5a26eddb9ff534c12e66ed82f9e
Ritik Kumar <ritik.kumar22@outlook.com>
2026-09-17 14:49:34 +0530

    feat: Implement user fetching and display in App component

--- day-159-docker-compose/frontend/src/app/App.jsx
+++ day-159-docker-compose/frontend/src/app/App.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { useState } from "react";
+import "./App.css";
+import { useEffect } from "react";
+import axios from "axios";
 
 const App = () => {
+
+  const [users, setUsers] = useState([])
+
+  const getUsers = async () => {
+    const response = await axios.get("/api/users");
+    setUsers(response.data);
+  }
+
+  useEffect(() => {
+    getUsers()
+  }, [])
+  
   return (
     <div>
-      App
+      <h1>Users</h1>
+      <ul>
+        {users.map((user) => {
+          return <li key={user.id}>{user.name}</li>
+        })}
+      </ul>
     </div>
   )
 }
