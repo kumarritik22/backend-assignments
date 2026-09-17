1a69ec9a9b8d6d889b7ea9db4592242f1964142c
Ritik Kumar <ritik.kumar22@outlook.com>
2026-09-17 14:36:14 +0530

    feat: initialize a project with setting up frontend and backend

---
+++ day-159-docker-compose/backend/server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import morgan from "morgan";
+
+const app = express();
+app.use(morgan("dev"));
+
+app.get("/api/health", (req, res) => {
+    res.status(200).json({
+        status: "OK"
+    });
+});
+
+app.get("/api/hello", (req, res) => {
+    res.status(200).json({
+        message: "Hello World!"
+    });
+});
+
+app.get("/api/users", (req, res) => {
+    const users = [
+        { id: 1, name: "Ramayan Part-1" },
+        { id: 2, name: "Ramayan Part-2" },
+        { id: 3, name: "Varanasi" }
+    ];
+
+    res.status(200).json(users);
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000")
+});
