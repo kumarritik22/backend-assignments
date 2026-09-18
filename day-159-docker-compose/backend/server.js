b63409530a7a44cf9e907f2b5b7510dcb81fa3cd
Ritik Kumar <ritik.kumar22@outlook.com>
2026-09-18 14:24:34 +0530

    Build frontend, and pasted frontend dist folder in backend and renamed it as public

--- day-159-docker-compose/backend/server.js
+++ day-159-docker-compose/backend/server.js
@@ -3,6 +3,7 @@ import morgan from "morgan";
 
 const app = express();
 app.use(morgan("dev"));
+app.use(express.static("public"));
 
 app.get("/api/health", (req, res) => {
     res.status(200).json({
@@ -24,6 +25,12 @@ app.get("/api/users", (req, res) => {
     ];
 
     res.status(200).json(users);
+});
+
+app.get("*name", (req, res) => {
+    res.sendFile("public/index.html", {
+        root: __dirname
+    });
 });
 
 app.listen(3000, () => {
