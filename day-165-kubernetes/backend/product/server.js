de9490dddaf62b41d7d60841852a5e0a1016fee3
Ritik Kumar <ritik.kumar22@outlook.com>
2026-09-22 11:43:53 +0530

    Create basic express server in product/backend folder.

---
+++ day-165-kubernetes/backend/product/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import morgan from "morgan";
+import axios from "axios";
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.get("/", async (req, res) => {
+    const response = await axios.get("http://express-service/")
+    res.send(response.data);
+})
+
+app.listen(8080, () => {
+    console.log("Server is running on port 8080.");
+});
